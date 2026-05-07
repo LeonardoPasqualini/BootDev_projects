364097d8d5d1ad53f6f248c304e1ed95299e7851
Leonardo Carvalho <lpleopasqualini@gmail.com>
2026-05-07 15:44:49 +0100

    Add asteroid-game folder as a normal directory

---
+++ asteroid-game/main.py
@@ -0,0 +1,56 @@
+import sys
+
+import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state, log_event
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    asteroid_field = AsteroidField()
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+            
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        # refresh screen
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
